d41ca6c1e8c2c84898667ce70965bea31184b6ff
Dimka <@>
2023-10-11 14:20:21 +0300

    gg

---
+++ WpfApp1/WpfApp1/S.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class S
+    {
+        public int s()
+        {
+            int a = 5;
+            int b = 6;
+            int S = a * b;
+            return S;
+        }
+
+    }
+}
